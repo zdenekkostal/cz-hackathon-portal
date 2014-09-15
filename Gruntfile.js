85ae82f23658c4c8d784d8b14f1b5c71029f8100
Adam Kloboucnik <a.kloboucnik@gmail.com>
2014-09-15 09:24:51 +0200

    Do not minify header image

--- Gruntfile.js
+++ Gruntfile.js
@@ -213,7 +213,7 @@ module.exports = function (grunt) {
                 files: [{
                     expand: true,
                     cwd: '<%= yeoman.app %>/images',
-                    src: '{,*/}*.{png,jpg,jpeg}',
+                    src: ['{,*/}*.{png,jpg,jpeg}', '!lame-blob.png'],
                     dest: '<%= yeoman.dist %>/images'
                 }]
             }
@@ -271,7 +271,8 @@ module.exports = function (grunt) {
                         '*.{ico,txt}',
                         '.htaccess',
                         'images/{,*/}*.{webp,gif}',
-                        'styles/fonts/*'
+                        'styles/fonts/*',
+                        'images/lame-blob.png'
                     ]
                 }]
             }
